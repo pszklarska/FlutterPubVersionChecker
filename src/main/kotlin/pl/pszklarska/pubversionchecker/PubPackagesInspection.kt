9370d5754efef0430c2d380ff60d5a0b65bfde16
pszklarska <pszklarska@gmail.com>
2019-05-13 16:08:08 +0200

    Code cleanup, version update to 1.0.1

--- src/main/kotlin/pl/pszklarska/pubversionchecker/PubPackagesInspection.kt
+++ src/main/kotlin/pl/pszklarska/pubversionchecker/PubPackagesInspection.kt
@@ -3,9 +3,9 @@ package pl.pszklarska.pubversionchecker
 import com.google.gson.Gson
 import com.intellij.codeInsight.daemon.GroupNames
 import com.intellij.codeInspection.LocalInspectionTool
-import com.intellij.codeInspection.LocalInspectionToolSession
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.psi.PsiElementVisitor
 import com.intellij.psi.PsiFile
 import com.intellij.util.containers.getIfSingle
@@ -13,9 +13,12 @@ import java.net.HttpURLConnection
 import java.net.URL
 import java.util.regex.Pattern
 
+
 const val REGEX_DEPENDENCY = ".*(?!version|sdk)\\b\\S+:.+\\.[0-9]+\\.[0-9]+"
 const val YML_EXTENSIONS = "yml"
 const val PUB_API_URL = "https://pub.dartlang.org/api/packages/"
+private val LOG = Logger.getInstance(PubPackagesInspection::class.java)
+
 
 class PubPackagesInspection : LocalInspectionTool() {
 
@@ -31,9 +34,7 @@ class PubPackagesInspection : LocalInspectionTool() {
         return "PubVersions"
     }
 
-    override fun buildVisitor(
-        holder: ProblemsHolder, isOnTheFly: Boolean, session: LocalInspectionToolSession
-    ): PsiElementVisitor {
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
         return YamlElementVisitor(holder)
     }
 
@@ -42,35 +43,33 @@ class PubPackagesInspection : LocalInspectionTool() {
     }
 }
 
-class YamlElementVisitor(
-    private val holder: ProblemsHolder
-) : PsiElementVisitor() {
+class YamlElementVisitor(private val holder: ProblemsHolder) : PsiElementVisitor() {
+
     override fun visitFile(file: PsiFile) {
         ApplicationManager.getApplication().runReadAction {
-            if (file.fileType.defaultExtension == YML_EXTENSIONS) {
-                file.readPackageLines().forEach {
-                    val dependency = it.first
-                    val counter = it.second
+            checkFile(file)
+        }
+    }
 
-                    val latestVersion = getLatestVersion(dependency)
-                    val currentVersion = getCurrentVersion(dependency)
+    private fun checkFile(file: PsiFile) {
+        if (file.isPubspecFile()) {
+            file.readPackageLines().forEach {
+                val dependency = it.first
+                val counter = it.second
 
-                    if (latestVersion.compareTo(currentVersion) != 0) {
-                        showProblem(file, counter, currentVersion, latestVersion)
-                    }
+                val latestVersion = getLatestVersion(dependency)
+                val currentVersion = getCurrentVersion(dependency)
+
+                if (latestVersion.compareTo(currentVersion) != 0) {
+                    holder.showProblem(file, counter, currentVersion, latestVersion)
                 }
             }
         }
     }
+}
 
-    private fun showProblem(
-        file: PsiFile, counter: Int, currentVersion: String, latestVersion: String
-    ) {
-        holder.registerProblem(
-            file.findElementAt(counter)!!,
-            "Version $currentVersion is different than the latest $latestVersion"
-        )
-    }
+private fun PsiFile.isPubspecFile(): Boolean {
+    return fileType.defaultExtension == YML_EXTENSIONS && name.contains("pubspec")
 }
 
 fun PsiFile.readPackageLines(): List<Pair<String, Int>> {
@@ -83,6 +82,7 @@ fun PsiFile.readPackageLines(): List<Pair<String, Int>> {
             line = line.trim()
             if (!line.startsWith("#") && line.isPackageName()) {
                 linesList.add(line to counter - 2)
+                printMessage("Found dependency: $line")
             }
             line = ""
         } else {
@@ -98,23 +98,43 @@ fun String.isPackageName(): Boolean {
 }
 
 fun getCurrentVersion(dependency: String): String {
-    return dependency.split(':')[1].replace("^", "").trim()
+    val currentVersion = dependency.split(':')[1].replace("^", "").trim()
+    printMessage("Current version: $currentVersion")
+    return currentVersion
 }
 
 fun getLatestVersion(line: String): String {
     val packageName = line.trim().split(':')[0]
     val url = URL(PUB_API_URL + packageName)
 
+    printMessage("Checking latest version for: $packageName")
+
     with(url.openConnection() as HttpURLConnection) {
         requestMethod = "GET"
         inputStream.bufferedReader().use {
             val response = parsePackageResponse(it.lines().getIfSingle()!!)
-            return response.latest.version.trim()
+            val latest = response.latest
+            val latestVersion = latest.version
+            val latestVersionTrim = latestVersion.trim()
+            printMessage("Latest version: $latestVersionTrim")
+            return latestVersionTrim
         }
     }
 
 }
 
+private fun printMessage(message: String) {
+    LOG.info(message)
+}
+
 fun parsePackageResponse(responseString: String): Response {
     return Gson().fromJson(responseString, Response::class.java)
+}
+
+private fun ProblemsHolder.showProblem(file: PsiFile, counter: Int, currentVersion: String, latestVersion: String) {
+    registerProblem(
+        file.findElementAt(counter)!!,
+        "Version $currentVersion is different than the latest $latestVersion"
+    )
+
 }
